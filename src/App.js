ab617c28fcc319f73bd4e97aa14863edc7c9eb74
Mir Umar Naeem <mirthepeer@gmail.com>
2022-11-25 05:49:52 -0500

    added loading display while feching data

--- src/App.js
+++ src/App.js
@@ -13,11 +13,12 @@ function App() {
   const [quizCards, setQuizCards] = useState([])
   
   console.log(quizCards)
+  console.log(quizState)
   
   function defaultQuizState(){
     return {
       quizActive: false,
-      loading: true,
+      loading: false,
       score: 0,
       showScore: false,
       quizEnded: false
@@ -34,23 +35,30 @@ function App() {
         setQuizData(data.results)
       )
     }
-     
+    
     
   },[quizState.quizActive])
 
   useEffect(()=>{
-    const quizCardContent = quizData.map(item => {
-      return {
-        id: uuid(),
-        question: item.question,
-        answers: [...item.incorrect_answers , item.correct_answer].sort((a, b) => 0.5 - Math.random()),
-        correct_answer: item.correct_answer,
-        selected: '' 
-
-      }
-    })
-
-    setQuizCards(quizCardContent)
+    if(quizData){
+      const quizCardContent = quizData.map(item => {
+        return {
+          id: uuid(),
+          question: item.question,
+          answers: [...item.incorrect_answers , item.correct_answer].sort((a, b) => 0.5 - Math.random()),
+          correct_answer: item.correct_answer,
+          selected: '' 
+  
+        }
+      })
+      
+      setQuizCards(quizCardContent)
+      setQuizState(prev=>{
+        return {...prev,loading:false}
+      })
+     
+    }
+    
 
   },[quizData])
 
@@ -67,7 +75,10 @@ function App() {
 
   function startQuiz(){
     setQuizState(prev=> ({...prev, quizActive:true}) )
-
+    setQuizState(prev=>{
+      return {...prev,loading:true}
+    })
+     
   }
 
  function checkAnswers(){
@@ -78,7 +89,7 @@ function App() {
     setQuizState(prev=> ({...prev, quizEnded:true,showScore:true, score: correctQuestion.length}) )
     
   } else{
-    setQuizState(defaultQuizState)
+    setQuizState(defaultQuizState())
   }
   
   
@@ -113,7 +124,7 @@ const quizCardsDisplay = quizCards.map(question=>{
       {!quizActive &&<p className='sub'>Test you gaming knowledge</p>}
       {!quizActive &&<button className='primary-btn' onClick={startQuiz}>Start Quiz</button>}
     </div>
-    
+    {loading? <h1 className='loading'>Loading...</h1>: ''}
     <div className="quiz">
       {quizActive && quizCardsDisplay}
       <div className='button'>
@@ -121,7 +132,7 @@ const quizCardsDisplay = quizCards.map(question=>{
         {showScore && <p><span>GREEN</span> = Correct Answer</p>} 
       </div>
       {showScore && <p className='score'>Score: {score}/{quizCardsDisplay.length}</p>}
-      {quizActive &&<button className='primary-btn' onClick={checkAnswers}>{showScore? 'New Quiz' : 'Check Answers'}</button>}
+      {(quizActive && !loading) &&<button className='primary-btn' onClick={checkAnswers}>{showScore? 'New Quiz' : 'Check Answers'}</button>}
     </div>
     </div>
     
