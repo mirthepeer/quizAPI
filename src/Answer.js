8911174c4ab2f8c23f72108dc3355f6ccc67aee8
Mir Umar Naeem <mirthepeer@gmail.com>
2022-11-26 22:08:57 -0500

    Had to change colors of components so they compliment the background img

--- src/Answer.js
+++ src/Answer.js
@@ -8,7 +8,7 @@ export default function Answer(props){
     }
     let style
     if(props.selected === props.answer){
-        style = {backgroundColor: ' rgb(180, 180, 217)', color:'black', fontWeight:'bold'}
+        style = {backgroundColor: ' rgb(180, 180, 217)', color:'rgba(255, 255, 255, 0.999)', fontWeight:'bold', border:'2px solid black'}
     }
     if(((props.selected === props.answer) && props.selected!==props.correctAnswer) && props.quizEnded){
         style = {backgroundColor: ' rgba(255, 0, 0, 0.744)', color:'rgba(255, 255, 255, 0.999)', fontWeight:'bold', border: '3px solid white'}
