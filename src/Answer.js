11bedbedcef534f90a70312774e2f756d59eed47
Mir Umar Naeem <mirthepeer@gmail.com>
2022-11-28 01:58:39 -0500

    Changed body height

--- src/Answer.js
+++ src/Answer.js
@@ -7,12 +7,15 @@ export default function Answer(props){
         return {__html: props.answer}
     }
     let style
+    // style for selected option before checking answers
     if(props.selected === props.answer){
         style = {backgroundColor: ' rgb(180, 180, 217)', color:'rgba(255, 255, 255, 0.999)', fontWeight:'bold', border:'2px solid black'}
     }
+    // style for incorrect answer once answers are checked
     if(((props.selected === props.answer) && props.selected!==props.correctAnswer) && props.quizEnded){
         style = {backgroundColor: ' rgba(255, 0, 0, 0.744)', color:'rgba(255, 255, 255, 0.999)', fontWeight:'bold', border: '3px solid white'}
     }
+    // style for correctly answered answer
     if(props.quizEnded && props.answer===props.correctAnswer){
         style= {backgroundColor: 'rgb(51, 161, 51)' , fontWeight: 'bold'}
     }
